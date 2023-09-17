6965df06ed4f98e1c6b33f768aacf911fb9dbf3b
Lucas A. Santos <lucas_ads@hotmail.com.br>
2023-09-17 13:02:27 -0400

    Mining rewards & transactions

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 const SHA256 = require("crypto-js/sha256");
 
+class Transaction{
+    constructor(fromAddress, toAddress, ammount){
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.ammount = ammount;
+    }
+}
+
 class Block{
-    constructor(index, timestamp, data, previousHash=''){
+    constructor(timestamp, transactions, previousHash=''){
         this.content = {};
-        this.content.index = index;
         this.content.timestamp = timestamp;
-        this.content.data = data;
+        this.content.transactions = transactions;
         this.content.nonce = 0;
         this.content.previousHash = previousHash;
         
@@ -35,10 +42,13 @@ class Blockchain{
         this.chain = [];
         this.difficulty=2;
         this.createGenesisBlock();
+
+        this.pedingTransacions = [];
+        this.miningReward = 100;
     }
 
     createGenesisBlock(){
-        let genesis = new Block(0, "01/01/2023", "Genesis Block", "0");
+        let genesis = new Block("01/01/2023", "Genesis Block", "0");
         console.log("Mining Genesis Block...");
         genesis.mineBlock(this.difficulty);
         this.chain.push(genesis);
@@ -48,10 +58,37 @@ class Blockchain{
         return this.chain[this.chain.length-1];
     }
 
-    addBlock(newBlock){
-        newBlock.content.previousHash = this.getLatestBlock().hash;
-        newBlock.mineBlock(this.difficulty);
-        this.chain.push(newBlock);
+    minePendingTransactions(miningRewardAddress){
+        let block = new Block(Date.now(), this.pedingTransacions, this.getLatestBlock().hash); 
+        block.mineBlock(this.difficulty);
+
+        this.chain.push(block);
+
+        this.pedingTransacions = [
+            new Transaction(null, miningRewardAddress, this.miningReward)
+        ];
+    }
+
+    createTransaction(transaction){
+        this.pedingTransacions.push(transaction);
+    }
+
+    getBalanceOfAddress(address){
+        let balance = 0;
+        
+        for(const block of this.chain){
+            for(const transaction of block.content.transactions){
+                if(transaction.fromAddress === address){
+                    balance -= transaction.ammount;
+                }
+
+                if(transaction.toAddress === address){
+                    balance += transaction.ammount;
+                }
+            }
+        }
+
+        return balance;
     }
 
     isChainValid(){
@@ -68,18 +105,43 @@ class Blockchain{
     }
 }
 
+function testaBlockchainComTransacoes(){
+    let myNewCoin = new Blockchain();
+
+    myNewCoin.createTransaction(new Transaction('address1', 'address2', 100));
+    myNewCoin.createTransaction(new Transaction('address2', 'address1', 50));
+
+    console.log("\n Starting the miner...");
+    myNewCoin.minePendingTransactions('xaviers-address');
+
+    console.log("\n Balance of xavier is ", myNewCoin.getBalanceOfAddress('xaviers-address'));
+    console.log("\n Balance of address1 is ", myNewCoin.getBalanceOfAddress('address1'));
+    console.log("\n Balance of address2 is ", myNewCoin.getBalanceOfAddress('address2'));
+
+    console.log("\n Starting the miner again...");
+    myNewCoin.minePendingTransactions('xaviers-address');
+
+    console.log("\n Balance of xavier is ", myNewCoin.getBalanceOfAddress('xaviers-address'));
+
+    console.log(JSON.stringify(myNewCoin.chain, null, 4));
+}
+
+testaBlockchainComTransacoes();
+
+//#######################################################
+
 function testaConsistenciaBlockchain(){
     
     let myNewCoin = new Blockchain();
     
     console.log("Mining Block 1...");
-    myNewCoin.addBlock( new Block(1, "02/02/2023", {ammount: 5} ));
+    myNewCoin.addBlock( new Block("02/02/2023", {ammount: 5} ));
 
     console.log("Mining Block 2...");
-    myNewCoin.addBlock( new Block(2, "03/02/2023", {ammount: 10} ));
+    myNewCoin.addBlock( new Block("03/02/2023", {ammount: 10} ));
 
     console.log("Mining Block 3...");
-    myNewCoin.addBlock( new Block(3, "04/02/2023", {ammount: 7} ));
+    myNewCoin.addBlock( new Block("04/02/2023", {ammount: 7} ));
     
     
     console.log(JSON.stringify(myNewCoin.chain, null, 4));
@@ -92,4 +154,4 @@ function testaConsistenciaBlockchain(){
     console.log(JSON.stringify(myNewCoin.chain, null, 4));
 }
 
-testaConsistenciaBlockchain();
+//testaConsistenciaBlockchain();
